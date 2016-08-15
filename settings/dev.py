7db7cd327b63130cbac4b15784b80a5d7b495c77
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-08-15 15:42:47 -0400

    add base settings

---
+++ settings/dev.py
@@ -0,0 +1,30 @@
+"""
+This file will provide the settings
+that we’ll use for our development environment.
+
+"""
+
+from base import *
+
+DEBUG = True
+
+INSTALLED_APPS.append('debug_toolbar')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Stripe environment variables
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_mttFwgc78Jk5ShtvskP2RHpG')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_k7CEJxI3Net5oB6i8zyT70Ya')
+
+
+# Paypal environment variables
+
+SITE_URL = 'http://127.0.0.1:8000'
+PAYPAL_NOTIFY_URL = ' http://127.0.0.1/a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = 'pramatha.madhavankutty@yahoo.com'
+
---
+++ settings/staging.py
@@ -0,0 +1,19 @@
+from base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Stripe environment variables
+STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_mttFwgc78Jk5ShtvskP2RHpG')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_k7CEJxI3Net5oB6i8zyT70Ya')
+
+# Paypal environment variables
+SITE_URL = '<your Heroku URL>'
+PAYPAL_NOTIFY_URL = '<your Heroku URL>/a-very-hard-to-guess-url/'
+PAYPAL_RECEIVER_EMAIL = 'pramatha.madhavankutty@yahoo.com'
