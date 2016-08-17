10b8f5863e6863437993b6a9c4b7f8bd7c9e0059
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-08-17 03:18:08 -0400

    modify

--- settings/staging.py
+++ settings/staging.py
@@ -12,8 +12,8 @@ DATABASES = {
 }
 
 DATABASES['default'] = dj_database_url.parse(
-    "mysql://b081e17dddde81:e3c0634d@eu-cdbr-west-01.cleardb.com/heroku_e6b7d755397dfa7?reconnect=true")
-
+    "mysql://bbf6d3302ef55c:5a72dd57@eu-cdbr-west-01.cleardb.com/heroku_a9f27171bd7bd62?reconnect=true"
+)
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_mttFwgc78Jk5ShtvskP2RHpG')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_k7CEJxI3Net5oB6i8zyT70Ya')
